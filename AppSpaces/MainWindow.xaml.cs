69bc90297335e0db2a1418bc074a2ad79e4155e6
Martijn Kooij <post@martijnkooij.nl>
2023-01-09 23:10:11 +0100

    Just getting started...

---
+++ AppSpaces/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows.Automation;
+
+namespace AppSpaces
+{
+	public partial class MainWindow
+	{
+		// ReSharper disable InconsistentNaming
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int Width, int Height, bool Repaint);
+		// ReSharper enable InconsistentNaming
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Log("Started.");
+
+			Automation.AddAutomationEventHandler(
+				eventId: WindowPattern.WindowOpenedEvent,
+				element: AutomationElement.RootElement,
+				scope: TreeScope.Children,
+				eventHandler: OnWindowOpened);
+
+			Log("Listening...");
+		}
+
+		private void OnWindowOpened(object sender, AutomationEventArgs e)
+		{
+			var element = sender as AutomationElement;
+			if (element == null) return;
+
+			Log($"Window opened for {element.Current.Name} > {element.Current.NativeWindowHandle} > {element.Current.ProcessId}");
+
+			if (element.Current.Name == "Visual Studio Code")
+			{
+				MoveWindow(new IntPtr(element.Current.NativeWindowHandle), 100, 100, 500, 500, true);
+
+				var lastError = Marshal.GetLastWin32Error();
+				Log($"Last error: {lastError}");
+			}
+		}
+
+		private void Log(string data)
+		{
+			Dispatcher.Invoke(() =>
+			{
+				Logger.Text = $"{data}{Environment.NewLine}{Logger.Text}";
+			});
+		}
+
+		protected override void OnClosing(CancelEventArgs e)
+		{
+			Log("Closing...");
+			Automation.RemoveAllEventHandlers();
+
+			base.OnClosing(e);
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # AppSpaces
 Desktop spaces for your apps
+
+## WiP
+This is once again just a PoC based on an idea that popped in my head. Whether I will finish this or not is to be seen...
+
+## Definitions
+- A space is an area on the screen.
+- An AppSpace in an application linked to an app and all its windows.
+- An AppSpace cannot contain any duplicate apps, 1 app can be placed in 1 space only.
+- You can define multiple AppSpaces
+
+## Use cases
+- When a new window is opened it should be placed in its designated space
+- When a window is dropped in a space it is maximized within that space.
+    - A question is asked whether the app should be linked to that space.
+        - If if was already linked to another space it is moved.
+- When an app is maximized it is maximized within the space the click was in. Optionally?
