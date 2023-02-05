3456a42120f5fb7880c4736e1ee281bbaae29657
Martijn Kooij <martijn.kooij@snappet.org>
2023-02-05 15:27:54 +0100

    Low FPS but it works...

--- AppSpaces.App/StreamingWindow.xaml.cs
+++ AppSpaces.App/StreamingWindow.xaml.cs
@@ -1,17 +1,20 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.Windows;
-using System.Windows.Interop;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Threading;
 using AppSpaces.App.Models;
-using Size = System.Drawing.Size;
+using PixelFormat = System.Drawing.Imaging.PixelFormat;
+using Rectangle = System.Drawing.Rectangle;
 
 namespace AppSpaces.App;
 
 public partial class StreamingWindow
 {
 	private readonly Space _streamingSpace;
+	private bool _isCapturing;
+	private DateTime _lastTime;
+	private int _framesRendered;
+	private int _fps;
 
 	public StreamingWindow(Space streamingSpace)
 	{
@@ -21,23 +24,54 @@ public partial class StreamingWindow
 
 		Title = Constants.StreamingWindowTitle;
 
-		var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.033) };
-		timer.Tick += TimerTick;
+		var timer = new HiResTimer(100f);
+		timer.Elapsed += TimerTick;
 		timer.Start();
 
 		Closed += (_, _) => timer.Stop();
 	}
 
-	private void TimerTick(object? sender, EventArgs e)
+	private void TimerTick()
 	{
-		var bmp = new Bitmap(_streamingSpace.Location.Width, _streamingSpace.Location.Height, PixelFormat.Format32bppArgb);
-		var g = Graphics.FromImage(bmp);
+		if (!Dispatcher.CheckAccess())
+		{
+			Dispatcher.Invoke(TimerTick);
+			return;
+		}
+
+		if (_isCapturing) return;
+		_isCapturing = true;
+		_framesRendered++;
+		if ((DateTime.Now - _lastTime).TotalMilliseconds >= 1000)
+		{
+			_fps = _framesRendered;                     
+			_framesRendered = 0;            
+			_lastTime = DateTime.Now;
+		}
+		Fps.Text = $"{_fps}FPS";
+
+		using var bmp = new Bitmap(_streamingSpace.Location.Width, _streamingSpace.Location.Height, PixelFormat.Format24bppRgb);
+		using var g = Graphics.FromImage(bmp);
 		g.CopyFromScreen(_streamingSpace.Location.X, _streamingSpace.Location.Y, 0, 0, new Size(_streamingSpace.Location.Width, _streamingSpace.Location.Height), CopyPixelOperation.SourceCopy);
 
-		StreamingImage.Source = Imaging.CreateBitmapSourceFromHBitmap(
-			bmp.GetHbitmap(),
-			IntPtr.Zero,
-			Int32Rect.Empty,
-			BitmapSizeOptions.FromWidthAndHeight(_streamingSpace.Location.Width, _streamingSpace.Location.Height));
+		StreamingImage.Source = Convert(bmp);
+		_isCapturing = false;
+	}
+
+	private static BitmapSource Convert(Bitmap bitmap)
+	{
+		var bitmapData = bitmap.LockBits(
+			new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+			ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+		var bitmapSource = BitmapSource.Create(
+			bitmapData.Width, bitmapData.Height,
+			bitmap.HorizontalResolution, bitmap.VerticalResolution,
+			PixelFormats.Bgr24, null,
+			bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+
+		bitmap.UnlockBits(bitmapData);
+
+		return bitmapSource;
 	}
 }
